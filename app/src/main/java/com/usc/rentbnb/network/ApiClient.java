0ea6e456e10a569013f781579dd7827758bfd590
Jules Lada <juleslada35@gmail.com>
2026-05-11 01:45:59 +0800

    style: created unfinished dashbaord UI

--- app/src/main/java/com/usc/rentbnb/network/ApiClient.java
+++ app/src/main/java/com/usc/rentbnb/network/ApiClient.java
@@ -9,14 +9,11 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
-    private static final String BASE_URL = "https://rentbnb-0wbk.onrender.com/api/v1/"; // http://10.0.2.2:3000/api/v1/ is localhost for Android emulator | https://rentbnb-0wbk.onrender.com/api/v1/ for Render
+    private static final String BASE_URL = "https://rentbnb-0wbk.onrender.com/api/v1/"; // http://10.0.2.2:3000 is localhost for Android emulator | https://rentbnb-0wbk.onrender.com for Render
     private static Retrofit retrofit = null;
     public static Retrofit getClient() {
         if (retrofit == null) {
             OkHttpClient okHttpClient = new OkHttpClient.Builder()
-                    .connectTimeout(30, TimeUnit.SECONDS) // Increase to 30s
-                    .readTimeout(30, TimeUnit.SECONDS)
-                    .writeTimeout(30, TimeUnit.SECONDS)
                     .addInterceptor(new AuthInterceptor())
                     .build();
 

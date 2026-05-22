8f7e3fa2f8d306476460fb8ea9d4147c76aafa5e
Jules Lada <juleslada35@gmail.com>
2026-05-22 17:05:54 +0800

    layout: styled thank you and digital receipt

--- app/src/main/java/com/usc/rentbnb/ui/booking/ThankYouPage.java
+++ app/src/main/java/com/usc/rentbnb/ui/booking/ThankYouPage.java
@@ -2,6 +2,7 @@ package com.usc.rentbnb.ui.booking;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
@@ -10,8 +11,8 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.usc.rentbnb.R;
 import com.usc.rentbnb.ui.home.HomeActivity;
+import com.usc.rentbnb.R;
 
 public class ThankYouPage extends AppCompatActivity {
 
@@ -20,16 +21,16 @@ public class ThankYouPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_thank_you);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.ivThankYouLogo).getRootView(), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
 
         Button btnContinue = findViewById(R.id.btnContinue);
-        btnContinue.setOnClickListener(v -> {
-            Intent nextIntent = new Intent(ThankYouPage.this, HomeActivity.class);
-            startActivity(nextIntent);
-        });
+        if (btnContinue != null) {
+            btnContinue.setOnClickListener(v -> {
+                // Return to the main entry point
+                Intent nextIntent = new Intent(ThankYouPage.this, HomeActivity.class);
+                nextIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(nextIntent);
+                finish();
+            });
+        }
     }
 }
